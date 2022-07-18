39b1b4172017ed6ba7102f0f2ff42f84aa97710e
HunterBowie <73845280+HunterBowie@users.noreply.github.com>
2022-07-18 13:43:33 -0700

    added react

--- index.js
+++ index.js
@@ -1,29 +1,12 @@
-const { readFile } = require('fs');
 const express = require('express');
 const app = express();
 
-
-
 app.use(express.json());
 
-const videos = [
-    {id: 1, name: "inception"},
-    {id: 2, name: "founder"},
-
-];
-
-app.get('/api/videos/:id', (req, res) => {
-    res.send(videos.find(v => v.id === parseInt(req.params.id)));
+app.get('/api', (req, res) => {
+    res.json({name: "server-109", data: [1, 4, 5, 2, 5]})
 });
 
-app.get('/', (req, res) => {
-    readFile('./index.html', 'utf8', (error, data) => {
-        res.send(data);
-    });
-    
-});
-
-
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`listening on port ${port}`));
 
